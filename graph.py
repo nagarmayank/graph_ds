e1ac6d8f09027225bce1f7cbf8e8861eb210c25f
Mayank Nagar <nagar.mayank@gmail.com>
2019-07-08 06:59:35 +0530

    Additional requirements

--- graph.py
+++ graph.py
@@ -7,6 +7,8 @@ class BSGraph(object):
         self.vertices = {}
         self.verticesList = [0]*size
         self.size = size
+        self.movies = []
+        self.actors = []
     
     def add_vertex(self, vertex, id):
         if vertex <= self.size-1:
@@ -26,8 +28,8 @@ class BSGraph(object):
         
     def readActMovfile(self, filename):
         ls = []
-        movies = []
-        actors = []
+        self.movies = []
+        self.actors = []
         with open(filename, 'r') as f:
             for x in f:
                 ls = x.split('/')
@@ -36,21 +38,55 @@ class BSGraph(object):
                     count = len(self.verticesList) - self.verticesList.count(0)
                     self.add_vertex(count, item)
                     if idx == 0:
-                        movies.append(item)
+                        self.movies.append(item)
                         movieName = item
                         #movieIndex = count
                         #self.add_edge(item, item)
                     else:
-                        actors.append(item)
+                        self.actors.append(item)
                         self.add_edge(movieName, item)
             
-        print(list(set(movies)))
-        print(list(set(actors)))
+        #print(list(set(movies)))
+        #print(list(set(actors)))
         return None
-
-
-mov = BSGraph(10)
+    
+    def displayActMov(self):
+        ''' This method is defined to list the movie names and actor list
+        in a text output file'''
+        
+        lines = []
+        lines.append("--------Function displayActMov--------")
+        lines.append("\nTotal number of movies: "+ 
+                     str(len(list(set(self.movies)))))
+        lines.append("\nTotal number of actors: "+ 
+                     str(len(list(set(self.actors)))))
+        
+        lines.append("\nList of movies:\n")
+        
+        for movie in list(set(self.movies)):
+            lines.append(movie)
+            lines.append("\n")
+        
+        lines.append("\n")
+        lines.append("List of actors:\n")
+        
+        for actor in list(set(self.actors)):
+            lines.append(actor)
+            lines.append("\n")
+        
+        lines.append("--------------------------------------")
+        
+        with open('outputPS2.txt', 'w') as f:
+            f.writelines(lines)
+        
+        return None
+    
+            
+mov = BSGraph(100)
 
 mov.readActMovfile("inputPS2.txt")
+mov.displayActMov()
 
+#print(mov.vertices)
+#print(mov.verticesList)
 #print(mov.edges)
