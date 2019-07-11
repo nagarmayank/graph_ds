cdf1713c55db4f5ba84e6e3c033f2ea3bd8e4834
Mayank Nagar <nagar.mayank@gmail.com>
2019-07-11 22:36:47 +0530

    Add files via upload

--- graph.py
+++ graph.py
@@ -9,6 +9,9 @@ class BSGraph(object):
         self.edges = [] # Adjacency Matrix
         self.first_call_displayMoviesOfActor = True
         self.first_call_displayActorsOfMovie = True
+        self.first_call_findMovieRelation = True
+        self.first_call_findMovieTransRelation = True
+
         for i in range(size):
             self.edges.append([0 for i in range(size)])
     
@@ -53,7 +56,7 @@ class BSGraph(object):
                 temp_ls = x.split('/')
                 for idx, item in enumerate(temp_ls):
                     item = item.strip("\n").strip(" ")
-                    count = len(self.verticesList) - self.verticesList.count(0) + 1
+                    count = len(self.verticesList) - self.verticesList.count(0)
                     self.add_vertex(count, item)
                     if idx == 0: # 1st item in the list represents movie
                         self.movies.append(item)
@@ -115,6 +118,16 @@ class BSGraph(object):
                     self.displayActorsOfMovie(line[13:].
                                                       strip(" ").
                                                       strip("\n"))
+                    
+                elif line.startswith('RMovies'):
+                    movieA = line.split(':')[1].strip(" ").strip("\n")
+                    movieB = line.split(':')[2].strip(" ").strip("\n")
+                    self.findMovieRelation(movieA, movieB)
+                
+                elif line.startswith('TMovies'):
+                    movieA = line.split(':')[1].strip(" ").strip("\n")
+                    movieB = line.split(':')[2].strip(" ").strip("\n")
+                    self.findMovieTransRelation(movieA, movieB)
             
         return None
     
@@ -132,7 +145,7 @@ class BSGraph(object):
         # Below code lists movies for the search Actor. If the actor is not 
         # found in list, then display error accordingly
         
-        if self.vertices.get(actor):
+        if self.vertices.get(actor, -1) != -1:
             lines_to_write.append("\n\nActor Name: "+ actor)
             lines_to_write.append("\nList of Movies:")
             for idx, item in enumerate(self.edges[self.vertices.get(actor)]):
@@ -143,7 +156,7 @@ class BSGraph(object):
                 else:
                     pass
         else:
-            lines_to_write.append("\n\n"+ actor+ " is not found in the list")
+            lines_to_write.append("\n\nActor Name: "+ actor+ " is not found in the list")
         
         # Write data to outfile
         with open('outputPS2.txt', 'a') as f:
@@ -161,7 +174,7 @@ class BSGraph(object):
         
         # Below code lists movies for the search Actor. If the actor is not 
         # found in list, then display error accordingly
-        if self.vertices.get(movie):
+        if self.vertices.get(movie, -1) != -1:
             lines_to_write.append("\n\nMovie Name: "+ movie)
             lines_to_write.append("\nList of Actors:")
             for idx, item in enumerate(self.edges[self.vertices.get(movie)]):
@@ -172,13 +185,118 @@ class BSGraph(object):
                 else:
                     pass
         else:
-            lines_to_write.append("\n\n"+ movie+ " is not found in the list")
+            lines_to_write.append("\n\nMovie Name: "+ movie+ " is not found in the list")
         
         # Write data to outfile
         with open('outputPS2.txt', 'a') as f:
             f.writelines(lines_to_write)
 
         return None
+    
+    
+    def findMovieRelation(self, movieA, movieB):
+        """This method uses Depth-First-Search traversal to traverse the 
+        graph and find relation between the passed 2 movie names"""
+        
+        lines_to_write = []
+        
+        if self.first_call_findMovieRelation:
+            lines_to_write.append("\n\n--------Function findMovieRelation --------")
+            self.first_call_findMovieRelation = False
+        
+        lines_to_write.append("\n\nMovie: "+movieA)
+        lines_to_write.append("\nMovie: "+movieB)
+        
+        movieA_id = self.vertices.get(movieA)
+        movieB_id = self.vertices.get(movieB)
+        
+        # Recursive call to dfs() method to get the complete traversal with
+        # movieA node as root. Recursion call is made only if movieA is in list
+        if self.vertices.get(movieA, -1) != -1:
+            
+            path = self.dfs(self.vertices.get(movieA), [])
+
+            # Traverse path until movieB is found and return related Actor
+            count = 0
+            for i in path:
+                count += 1
+                if i == movieB_id:
+                    related = previous
+                    final_count = count
+                previous = i
+    
+            # Two movies are related by an actor, hence, final count should be 3
+            if final_count == 3:
+                lines_to_write.append("\nRelated: Yes, "+ 
+                                    list(self.vertices.keys())[
+                                    list(self.vertices.values()).index(related)])
+            else:
+                lines_to_write.append("\nRelated: No")
+        else:
+            lines_to_write.append("\nRelated: Movie %s is not found" %movieA)
+        
+        # Write to output file
+        with open('outputPS2.txt', 'a') as f:
+            f.writelines(lines_to_write)
+            
+        return None
+    
+    def findMovieTransRelation(self, movieA, movieB):
+        """This method uses Depth-First-Search traversal to traverse the 
+        graph and find relation between the passed 2 movie names"""
+        
+        lines_to_write = []
+        
+        if self.first_call_findMovieTransRelation:
+            lines_to_write.append("\n\n--------Function findMovieTransRelation --------")
+            self.first_call_findMovieTransRelation = False
+        
+        lines_to_write.append("\n\nMovie: "+movieA)
+        lines_to_write.append("\nMovie: "+movieB)
+        
+        movieA_id = self.vertices.get(movieA)
+        movieB_id = self.vertices.get(movieB)
+        
+        # Recursive call to dfs() method to get the complete traversal with
+        # movieA node as root. Recursion call is made only if movieA is in list
+        if self.vertices.get(movieA, -1) != -1:
+            path = self.dfs(self.vertices.get(movieA), [])
+        
+            traverse = ""
+            final_path = ""
+            
+            # Traverse path until movieB is found and return final path
+            for i in path:
+                traverse += list(self.vertices.keys())[
+                                    list(self.vertices.values()).index(i)] + " > "
+                if i == movieB_id:
+                    final_path = traverse[0:-3]
+            
+            # If length of final path is 0, then there is no relation
+            if len(final_path) > 0:
+                lines_to_write.append("\nRelated: Yes, "+ final_path)
+            else:
+                lines_to_write.append("\nRelated: No")
+        else:
+            lines_to_write.append("\nRelated: Movie %s is not found" %movieA)
+        
+        # Write to outfile
+        with open('outputPS2.txt', 'a') as f:
+            f.writelines(lines_to_write)
+            
+        return None
+    
+    
+    def dfs(self, vertex, path=[]):
+        """This is a method defined which is called recursively to traverse 
+        using Adjacency Matrix"""
+        path += [vertex]
+                
+        for idx, neighbour in enumerate(self.edges[vertex]):
+            if neighbour and idx not in path:
+                path = self.dfs(idx, path)
+        return path
+               
 
 if __name__ == "__main__":
 
@@ -193,5 +311,4 @@ if __name__ == "__main__":
     
     # Reads prompt file and executes multiple functions
     movAct.readPromptFile("promptsPS2.txt")
-    
-    
+        
